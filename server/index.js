54144f482a93dd3b2f1c87e48e26826292d52195
Kamogelobradley200 <kamogelobradley200@gmail.com>
2026-04-05 06:51:09 +0200

    update: add CI workflows for client and server, update deployment configurations, and manage package dependencies

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const usersRouter = require('./src/routes/usersRoute');
 
-
 const app = express();
 const port = 3000;
 app.use(express.json());
+
+// setup CORS to allow requests from SWA and local development
+app.use(cors({
+  origin: [
+    'https://nice-water-0d3098403.1.azurestaticapps.net', // replace with real SWA URL
+    'http://localhost:5173'
+  ]
+}));
 
 // Example route
 
