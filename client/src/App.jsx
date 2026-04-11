8dd76a3e8f2c37939ecf6ef8a14c6bf82f71f641
Karabocode <tlakakarabo98@gmail.com>
2026-04-11 21:33:38 +0200

    refactor: remove SignIn component and page; update App to only render SignUpPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,11 @@
 
 import './App.css'
 import SignUpPage from './pages/SignUpPage';
-import SignInPage from './pages/SignInPage';
 
 function App() {
 
   return (
     <>
-    <SignInPage />
     <SignUpPage />
     </>
   )
