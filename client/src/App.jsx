baf83d01b30bc7695c7d4a79bb6c6d1b6a74c5f7
Mpho10111 <mphomurashiwa@gmail.com>
2026-04-16 02:18:14 +0200

    feat: add seller profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import SignUpPage from './pages/SignUpPage';
 import StaffDashboard from './pages/StaffDashboard';
 import ModifyDetailsPage from './pages/ModifyDetailsPage';
 import StudentDashboard  from './pages/StudentDashboard';
+import SellerProfilePage from './pages/SellerProfilePage';
 
 
 function App() {
@@ -46,6 +47,7 @@ function App() {
           <Route path="/staff-dashboard" element={<StaffDashboard />} />
           <Route path="/modify-details" element={<ModifyDetailsPage />} />
           <Route path="/student-dashboard" element={<StudentDashboard/>} />
+          <Route path="/seller-profile" element={<SellerProfilePage />} />
         </Routes>
       </BrowserRouter>
     </>
