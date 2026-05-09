be64ff91fa36da30e97b9b911112c247de8f6ec0
Mpho10111 <mphomurashiwa@gmail.com>
2026-05-09 17:54:30 +0200

    feat: add notifications page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { supabase } from './config/supabaseClient';
-import './App.css'
-import WelcomePage from './pages/WelcomePage'
-import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignUpPage';
-import StaffDashboard from './pages/StaffDashboard';
-import StudentDashboard  from './pages/StudentDashboard';
-import ListingDetails from './pages/ListingDetails';
-import CreateListing from './pages/CreateListing';
-import ChatPage from './pages/ChatPage';
-import SellerProfilePage from './pages/SellerProfilePage';
-import MessagesPage from './pages/MessagesPage';
-import NotificationsPage from './pages/NotificationsPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { supabase } from "./config/supabaseClient";
+import "./App.css";
+import WelcomePage from "./pages/WelcomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import StaffDashboard from "./pages/StaffDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import ListingDetails from "./pages/ListingDetails";
+import CreateListing from "./pages/CreateListing";
+import ChatPage from "./pages/ChatPage";
+import SellerProfilePage from "./pages/SellerProfilePage";
+import MessagesPage from "./pages/MessagesPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
   const [session, setSession] = useState(null);
@@ -25,42 +26,41 @@ function App() {
 
     getSession();
 
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      },
+    );
 
     return () => listener.subscription.unsubscribe();
   }, []);
 
-    useEffect(() => {
+  useEffect(() => {
     if (session) {
-      console.log("Logged in:", session.user.email);
+      console.log("Logged in:", session.user);
     } else {
       console.log("No user logged in");
     }
   }, [session]);
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/staff-dashboard" element={<StaffDashboard />} />
-          <Route path="/student-dashboard" element={<StudentDashboard/>} />
-          <Route path="/listing/:id" element={<ListingDetails />} />
-          <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/chat/:id" element={<ChatPage/>}/>
-          <Route path="/seller-profile" element={<SellerProfilePage />} />
-          <Route path="/messages" element={<MessagesPage />} />
-          <Route path="/notifications" element={<NotificationsPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
-
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/staff-dashboard" element={<StaffDashboard />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/listing/:id" element={<ListingDetails />} />
+        <Route path="/create-listing" element={<CreateListing />} />
+        <Route path="/chat/:id" element={<ChatPage />} />
+        <Route path="/seller-profile" element={<SellerProfilePage />} />
+        <Route path="/messages" element={<MessagesPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
