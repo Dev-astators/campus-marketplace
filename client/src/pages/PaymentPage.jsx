a226a9eda76915320617872bf308208239acd06a
Tjeding <makubungtjeding@gmail.com>
2026-05-18 13:49:23 +0200

    b

--- client/src/pages/PaymentPage.jsx
+++ client/src/pages/PaymentPage.jsx
@@ -31,7 +31,7 @@ export default function PaymentPage({ result }) {
       let viteSandbox = false;
       try {
         viteSandbox = import.meta?.env?.VITE_PAYFAST_SANDBOX === 'true';
-      } catch (e) {
+      } catch {
         viteSandbox = false;
       }
 
